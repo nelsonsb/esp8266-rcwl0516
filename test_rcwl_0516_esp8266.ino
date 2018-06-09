8f0efa2627ae07b4f9f712d92b6a77b8e5ea9886
Moviltracing <nelsonsb@users.noreply.github.com>
2018-06-08 19:53:10 -0500

    Add files via upload

---
+++ test_rcwl_0516_esp8266.ino
@@ -0,0 +1,13 @@
+#define SensorPin 0
+#define LedPin    2
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SensorPin, INPUT);
+  pinMode(LedPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LedPin,digitalRead(SensorPin));
+}
